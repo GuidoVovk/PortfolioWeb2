00e394878b6770f0531aaa2a507284784ad2002c
GuidoVovk <guido_z86@hotmail.com>
2022-12-13 23:30:11 -0300

    login

--- src/main/java/com/portfolioWeb/Guido/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/portfolioWeb/Guido/security/jwt/JwtEntryPoint.java
@@ -9,19 +9,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
-
+    
     private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
-            
+    
+    
     @Override
     public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
-        logger.error("fail en el metodo commece");
+        logger.error("error en el método commence");
         res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
+
+
+   
     
 }
 
